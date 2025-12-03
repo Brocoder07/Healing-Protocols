b44c62c6e79af57b224f926821ca4e6cc1721c58
Akshay Manjunath <157276831+Brocoder07@users.noreply.github.com>
2025-12-03 16:19:35 +0530

    Refactor AcupunctureApi to use suspend function

--- Frontend/Clinic/app/src/main/java/com/deploy/accu_project/AcupunctureApi.kt
+++ Frontend/Clinic/app/src/main/java/com/deploy/accu_project/AcupunctureApi.kt
@@ -1,11 +1,9 @@
 package com.deploy.accu_project
 
-import okhttp3.ResponseBody
 import retrofit2.http.GET
 import retrofit2.http.Query
-import retrofit2.Call
 
-interface AcupunctureApi {
-    @GET("/search")//search endpoint
-    fun searchAcupuncture(@Query("query") query: String): Call<ResponseBody>
+interface AcupunctureApi{
+    @GET("/search")
+    suspend fun searchAcupuncture(@Query("query") query: String): List <AcupunctureResponse>
 }
